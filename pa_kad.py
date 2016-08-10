3c23336531c085c52f29c284f97d5ba6c5f40488
saeveritt <saeveritt@gmail.com>
2016-08-10 02:18:21 -0500

    Update pa_kad.py

--- pa_kad.py
+++ pa_kad.py
@@ -1,34 +1,34 @@
 from kad import DHT
 from time import sleep
-from subprocess import Popen, PIPE
 from hashlib import sha256
-from peerassets import issue_tx, verify_owner
+from peerassets import proven_txid, verify_owner
 from operator import xor
 
 
-host1,port1 = 'localhost',1234  #seed address and port
+host1,port1 = 'seed_address',1234  #seed address and port
 host2,port2 = 'localhost',1235  #your address and port
 
-data = Popen(['ppcoind','getnewaddress'],stdout=PIPE,stderr=PIPE)
-address,stderr = data.communicate()
+address = 'public address in ownership of required PeerAsset'
 address256 = sha256(address).hexdigest()
 
-#Requires address to have 200 quantity of hopium in order to be considered as a valid entry
-issuer = 'token issuer address'
+# Requires address to have a minimum quantity of 200 hopium in order to be considered as a valid entry
+issuer = 'token issuer public address'
 name = 'hopium' 
 qnt =  200
 
+# Returns True or False whether or not a given address owns the minumum amount of required PeerAsset
 test = verify_owner(address,issuer, name, qnt)
 
 if test == True:
         # Creating transaction by sending peerasset to newly created address and returns txid
-        txid = issue_tx(address,'name:%s,qnt:%s'%(name,qnt))
+        # proven_txid() returns the transaction id stating peerasset ownership
+        # For an entry to be valid there must be X amount of Y peerasset in address. This transaction will be signed by owner. 
+        txid = proven_txid(address,'issuer:%s,name:%s,qnt:%s'%(issuer,name,qnt))
 else:
-        print('Address does not own %s units of %s'%(qnt,name))
+        print('%s does not own %s units of %s issued by %s'%(address,qnt,name,issuer))
 
-#For an entry to be valid there must be X amount of Y peerasset in address. This will transaction will be signed by owner. 
 nodeid = xor(txid,address256)
-
+# node can be found by taking the xor of  the proven txid hash and hash of public address
 dht = DHT(host2,port2,id=nodeid,seeds=[(host1,port1)])
 
 #append some data to dht
